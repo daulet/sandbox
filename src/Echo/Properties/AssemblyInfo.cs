7c7353a3fe15b1ff98d2fbf6cec0c0336f1b9975
Daulet <daulet@users.noreply.github.com>
2017-01-16 23:28:58 -0800

    Add License, Nuget spec and downgrade to .Net 4.5 (#4)

--- src/Echo/Properties/AssemblyInfo.cs
+++ src/Echo/Properties/AssemblyInfo.cs
@@ -1,3 +1,9 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyCompany("Daulet Zhanguzin")]
+[assembly: AssemblyDescription("Record your code's execution and replay them in a test")]
+[assembly: AssemblyTitle("Echo: record and replay")]
+[assembly: AssemblyVersion("0.1")]
 
 [assembly: InternalsVisibleTo("Echo.UnitTests")]
