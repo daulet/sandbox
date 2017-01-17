174c82a3b1929b7dcfa4728366bdcf6bbecc68d8
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-01-17 00:27:28 -0800

    Loosen dependency requirements

--- src/Echo/Properties/AssemblyInfo.cs
+++ src/Echo/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Daulet Zhanguzin")]
 [assembly: AssemblyDescription("Record your code's execution and replay them in a test")]
 [assembly: AssemblyTitle("Echo: record and replay")]
-[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyVersion("0.1.2")]
 
 [assembly: InternalsVisibleTo("Echo.UnitTests")]
