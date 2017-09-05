a264e627d566f7a6a51b72a04bed8c03183d1e58
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-09-04 19:11:02 -0700

    Sign the assembly

--- src/Echo/Properties/AssemblyInfo.cs
+++ src/Echo/Properties/AssemblyInfo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Echo.UnitTests")]
+// & 'C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\x64\sn.exe' -Tp .\Echo.UnitTests.dll
+[assembly: InternalsVisibleTo("Echo.UnitTests, PublicKey=002400000c800000940000000602000000240000525341310004000001000100559d0f061f103a0a6b4c49c80aac5623f58540b417378eb6d071a2c2746a5892e586e362e45ce8bdc6c7dc7f349f019bbc245ce2264139c54d8965c81106964ca8ff903dd7539aadce8e12a669ae74c784b9adad9c01f686575d0c001221728dc0bec61c03d1c8e05c2eeca00fb644295416c05ee4e1868e9b4c9ddcb12799d9")]
