21bbfb620ea1779603bf042720e100f09529edbf
Daulet Zhanguzin <daulet@zhanguzin.kz>
2017-09-04 23:03:26 -0700

    Switch from custom Sign configuration to Release since it comes with optimizations.

--- script/release.ps1
+++ script/release.ps1
@@ -2,4 +2,4 @@ param(
     [string]$rootPath
 )
 
-msbuild.exe $rootPath\src\echo\Echo.csproj '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /nodeReuse:false /nologo /p:TreatWarningsAsErrors=true /p:Configuration=Sign
+msbuild.exe $rootPath\src\echo\Echo.csproj '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /nodeReuse:false /nologo /p:TreatWarningsAsErrors=true /p:Configuration=Release
