9db21f70db766b2ecfd3713abaeae098083dceaf
Daulet <daulet@zhanguzin.kz>
2017-09-09 20:26:47 -0700

    Clean up usings

--- src/Echo/Logging/MultiLogger.cs
+++ src/Echo/Logging/MultiLogger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Echo.Logging
+﻿namespace Echo.Logging
 {
     internal class MultiLogger : ILogger
     {
