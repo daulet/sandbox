5c9ee657b84bc45a971bc34f3623c82dc2cd57e1
Daulet <daulet@zhanguzin.kz>
2017-09-10 21:33:09 -0700

    Add more communication to build.ps1

--- script/build.ps1
+++ script/build.ps1
@@ -2,11 +2,15 @@ param(
     [string]$rootPath
 )
 
+Write-Host "Running clean" -ForegroundColor Blue
+
 . $PsScriptRoot\clean.ps1 $rootPath
 
 if ($LastExitCode -ne 0) {
     return $LastExitCode
 }
+
+Write-Host "Running restore" -ForegroundColor Blue
 
 . $PsScriptRoot\restore.ps1
 
@@ -14,10 +18,14 @@ if ($LastExitCode -ne 0) {
     return $LastExitCode
 }
 
+Write-Host "Running build" -ForegroundColor Blue
+
 msbuild.exe .\Echo.sln '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /nodeReuse:false /nologo /p:TreatWarningsAsErrors=true
 
 if ($LastExitCode -ne 0) {
     return $LastExitCode
 }
+
+Write-Host "Running test" -ForegroundColor Blue
 
 . $PsScriptRoot\test.ps1 $rootPath
