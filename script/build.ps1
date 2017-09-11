4ac40824f15c9d45279f829ccca4e40ea4745399
Daulet <daulet@zhanguzin.kz>
2017-09-10 21:11:10 -0700

    Make build script terminate early on failure

--- script/build.ps1
+++ script/build.ps1
@@ -1,1 +1,23 @@
+param(
+    [string]$rootPath
+)
+
+. $PsScriptRoot\clean.ps1 $rootPath
+
+if ($LastExitCode -ne 0) {
+    return $LastExitCode
+}
+
+. $PsScriptRoot\restore.ps1
+
+if ($LastExitCode -ne 0) {
+    return $LastExitCode
+}
+
 msbuild.exe .\Echo.sln '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /nodeReuse:false /nologo /p:TreatWarningsAsErrors=true
+
+if ($LastExitCode -ne 0) {
+    return $LastExitCode
+}
+
+. $PsScriptRoot\test.ps1 $rootPath
