a22e1743645e5c80b528735c3621fdab4bf57e4f
Daulet <daulet@zhanguzin.kz>
2017-09-13 23:20:42 -0700

    Bump version to 0.2.0

--- script/release.ps1
+++ script/release.ps1
@@ -1,5 +1,13 @@
 param(
-    [string]$rootPath
+    [string]$rootPath,
+    [string]$version
 )
 
+Write-Host "Running build" -ForegroundColor Blue
+
 msbuild.exe $rootPath\src\echo\Echo.csproj '/consoleLoggerParameters:Summary;Verbosity=minimal' /m /nodeReuse:false /nologo /p:TreatWarningsAsErrors=true /p:Configuration=Release
+
+Write-Host "Publishing to Nuget" -ForegroundColor Blue
+
+$apiKey = Get-Content "$rootPath\..\vault\keys\nuget\echo.apikey"
+nuget push "$rootPath\src\echo\bin\Release\Echo.$version.nupkg" $apiKey -source https://api.nuget.org/v3/index.json
