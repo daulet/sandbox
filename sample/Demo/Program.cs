fb209b6a3fefc2d32fc16dc62d2cfc588df14c97
Daulet <daulet@zhanguzin.kz>
2017-11-04 00:52:55 -0700

    Add provisioning failure recording to integration tests

--- sample/Demo/Program.cs
+++ sample/Demo/Program.cs
@@ -11,85 +11,8 @@ namespace Samples.Demo
         private static void Main(string[] args)
         {
             Console.WriteLine("Recording");
-            
-            using (var output = new StreamWriter("ProvisioningFails.echo"))
-            {
-                Record_ProvisioningFails_RefundIsMade(output);
-            }
 
             Console.ReadKey();
-        }
-
-        private static void Record_ProvisioningFails_RefundIsMade(StreamWriter streamWriter)
-        {
-            // Arrange
-
-            var billingMock = new Mock<IBilling>();
-            billingMock
-                .Setup(x => x.GetQuote(It.Is<QuoteRequest>(
-                        quoteRequest => quoteRequest.Service == ServiceType.Entertainment)
-                ))
-                .Returns(new QuoteResponse()
-                {
-                    Price = 10.50,
-                });
-
-            billingMock
-                .Setup(x => x.Charge(It.IsAny<PaymentRequest>()))
-                .Returns(new PaymentResponse()
-                {
-                    Result = PaymentCode.Success,
-                });
-
-            var providerMock = new Mock<IProvider>();
-            providerMock
-                .Setup(x => x.Provision(It.IsAny<ProvisioningRequest>()))
-                .Throws(new ProvisioningFailureException());
-
-            var purchaseRequest = new PurchaseRequest()
-            {
-                Customer = new User()
-                {
-                    FullName = "John Smith",
-                    Identifier = Guid.NewGuid(),
-                },
-                Payment = new CreditCardPaymentInstrument()
-                {
-                    CardExpirationDate = DateTime.UtcNow.AddYears(1),
-                    CardNumber = long.MaxValue,
-                    CardOwner = "John Smith SR",
-                    CardProvider = CreditCardProvider.Visa,
-                },
-                ServiceType = ServiceType.Entertainment,
-            };
-
-            // Act
-
-            try
-            {
-                Record(streamWriter, billingMock.Object, providerMock.Object, purchaseRequest);
-            }
-            catch (PurchaseFailureException)
-            {
-                // the Purchase is expected to fail since IProvider failed
-            }
-        }
-
-        private static void Record(StreamWriter streamWriter, IBilling billing, IProvider provider, PurchaseRequest purchaseRequest)
-        {
-            // setup recording
-
-            var writer = new EchoWriter(streamWriter);
-            var recorder = new Recorder(writer);
-
-            var recordedBilling = recorder.GetRecordingTarget<IBilling>(billing);
-            var recordedProvider = recorder.GetRecordingTarget<IProvider>(provider);
-            var actualEndpoint = new Endpoint(recordedBilling, recordedProvider);
-            var recordedEndpoint = recorder.GetRecordingTarget<IEndpoint>(actualEndpoint);
-
-            // Act
-
-            recordedEndpoint.Purchase(purchaseRequest);
         }
     }
 }
--- test/Echo.IntegrationTests/Recording/RecordingTests.cs
+++ test/Echo.IntegrationTests/Recording/RecordingTests.cs
@@ -12,14 +12,14 @@ namespace Echo.IntegrationTests.Recording
         [Fact]
         public void Purchase_RecordHappyCase_MatchesEmbeddedResource()
         {
-            var echoFileName = "Echo.IntegrationTests.Recording.HappyCase.echo";
+            const string echoFileName = "Echo.IntegrationTests.Recording.HappyCase.echo";
 
             // Arrange
 
             var billingMock = new Mock<IBilling>();
             billingMock
                 .Setup(x => x.GetQuote(It.Is<QuoteRequest>(
-                        quoteRequest => quoteRequest.Service == ServiceType.Entertainment)
+                    quoteRequest => quoteRequest.Service == ServiceType.Entertainment)
                 ))
                 .Returns(new QuoteResponse()
                 {
@@ -38,7 +38,7 @@ namespace Echo.IntegrationTests.Recording
                 .Setup(x => x.Provision(It.IsAny<ProvisioningRequest>()))
                 .Returns(new ProvisioningResponse()
                 {
-                    ProvisionedServices = new[] { ServiceType.Entertainment, ServiceType.Laundry, },
+                    ProvisionedServices = new[] {ServiceType.Entertainment, ServiceType.Laundry,},
                 });
 
             // write all echoes to a file
@@ -76,37 +76,20 @@ namespace Echo.IntegrationTests.Recording
 
             // Assert
 
-            string expectedEcho;
-            using (var expectedStreamReader = new StreamReader(echoFileName))
-            {
-                expectedEcho = expectedStreamReader.ReadToEnd()
-                    .Replace(Environment.NewLine, "\n");
-            }
-
-            string actualEcho;
-            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(echoFileName))
-            {
-                using (var actualStreamReader = new StreamReader(resourceStream))
-                {
-                    actualEcho = actualStreamReader.ReadToEnd()
-                        .Replace(Environment.NewLine, "\n");
-                }
-            }
-
-            Assert.Equal(expectedEcho, actualEcho);
+            Assert.Equal(ReadFile(echoFileName), ReadResource(echoFileName));
         }
 
         [Fact]
         public void Purchase_RecordBillingFails_MatchesEmbeddedResource()
         {
-            var echoFileName = "Echo.IntegrationTests.Recording.BillingFails.echo";
+            const string echoFileName = "Echo.IntegrationTests.Recording.BillingFails.echo";
 
-           // Arrange
+            // Arrange
 
             var billingMock = new Mock<IBilling>();
             billingMock
                 .Setup(x => x.GetQuote(It.Is<QuoteRequest>(
-                        quoteRequest => quoteRequest.Service == ServiceType.Entertainment)
+                    quoteRequest => quoteRequest.Service == ServiceType.Entertainment)
                 ))
                 .Returns(new QuoteResponse()
                 {
@@ -164,24 +147,102 @@ namespace Echo.IntegrationTests.Recording
 
             // Assert
 
-            string expectedEcho;
-            using (var expectedStreamReader = new StreamReader(echoFileName))
-            {
-                expectedEcho = expectedStreamReader.ReadToEnd()
-                    .Replace(Environment.NewLine, "\n");
-            }
+            Assert.Equal(ReadFile(echoFileName), ReadResource(echoFileName));
+        }
 
-            string actualEcho;
-            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(echoFileName))
-            {
-                using (var actualStreamReader = new StreamReader(resourceStream))
+        [Fact]
+        public void Purchase_RecordProvisioningFails_MatchesEmbeddedResource()
+        {
+            const string echoFileName = "Echo.IntegrationTests.Recording.ProvisioningFails.echo";
+
+            // Arrange
+
+            var billingMock = new Mock<IBilling>();
+            billingMock
+                .Setup(x => x.GetQuote(It.Is<QuoteRequest>(
+                        quoteRequest => quoteRequest.Service == ServiceType.Entertainment)
+                ))
+                .Returns(new QuoteResponse()
                 {
-                    actualEcho = actualStreamReader.ReadToEnd()
-                        .Replace(Environment.NewLine, "\n");
+                    Price = 10.50,
+                });
+
+            billingMock
+                .Setup(x => x.Charge(It.IsAny<PaymentRequest>()))
+                .Returns(new PaymentResponse()
+                {
+                    Result = PaymentCode.Success,
+                });
+
+            var providerMock = new Mock<IProvider>();
+            providerMock
+                .Setup(x => x.Provision(It.IsAny<ProvisioningRequest>()))
+                .Throws(new ProvisioningFailureException());
+
+            // write all echoes to a file
+            using (var output = new StreamWriter(echoFileName))
+            {
+                // setup recording
+
+                var writer = new EchoWriter(output);
+                var recorder = new Recorder(writer);
+
+                var recordedBilling = recorder.GetRecordingTarget<IBilling>(billingMock.Object);
+                var recordedProvider = recorder.GetRecordingTarget<IProvider>(providerMock.Object);
+                var actualEndpoint = new Endpoint(recordedBilling, recordedProvider);
+                var recordedEndpoint = recorder.GetRecordingTarget<IEndpoint>(actualEndpoint);
+
+                // Act
+
+                try
+                {
+                    recordedEndpoint.Purchase(new PurchaseRequest()
+                    {
+                        Customer = new User()
+                        {
+                            FullName = "John Smith",
+                            Identifier = new Guid("43fe7aaa-7be9-46e0-87e7-63c9a6f3a2ad"),
+                        },
+                        Payment = new CreditCardPaymentInstrument()
+                        {
+                            CardExpirationDate = DateTime.MaxValue,
+                            CardNumber = long.MaxValue,
+                            CardOwner = "John Smith SR",
+                            CardProvider = CreditCardProvider.Visa,
+                        },
+                        ServiceType = ServiceType.Entertainment,
+                    });
+                }
+                catch (PurchaseFailureException)
+                {
+                    // the Purchase is expected to fail since IProvider failed
                 }
             }
 
-            Assert.Equal(expectedEcho, actualEcho);
+            // Assert
+
+            Assert.Equal(ReadFile(echoFileName), ReadResource(echoFileName));
+        }
+
+        private static string ReadFile(string filename)
+        {
+            using (var expectedStreamReader = new StreamReader(filename))
+            {
+                return expectedStreamReader.ReadToEnd()
+                    .Replace(Environment.NewLine, "\n");
+            }
+        }
+
+        private static string ReadResource(string resourceName)
+        {
+            using (var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+            {
+                using (var actualStreamReader = new StreamReader(resourceStream))
+                {
+                    return actualStreamReader.ReadToEnd()
+                        .Replace(Environment.NewLine, "\n");
+                }
+            }
         }
     }
 }
