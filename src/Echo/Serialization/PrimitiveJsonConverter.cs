0e187656384f89ac9b99317d7c1e799ecbb3c667
Daulet <daulet@zhanguzin.kz>
2017-11-04 19:47:19 -0800

    Remove dependency on JavaScriptSerializer
    Move to Newtonsoft.Json completely

---
+++ src/Echo/Serialization/PrimitiveJsonConverter.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Echo.Serialization
+{
+    [ComVisible(false)]
+    public sealed class PrimitiveJsonConverter : JsonConverter
+    {
+        public override bool CanRead => false;
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType.IsPrimitive;
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            switch (serializer.TypeNameHandling)
+            {
+                case TypeNameHandling.All:
+                    writer.WriteStartObject();
+
+                    writer.WritePropertyName("$type", false);
+                    writer.WriteValue(value.GetType().FullName);
+                    
+                    writer.WritePropertyName("$value", false);
+                    writer.WriteValue(value);
+
+                    writer.WriteEndObject();
+                    break;
+                default:
+                    writer.WriteValue(value);
+                    break;
+            }
+        }
+    }
+}
